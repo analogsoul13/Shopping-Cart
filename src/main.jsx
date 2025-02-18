207745e037981e9bb649bc5c66d87d772050b5cb
analogsoul13 <analogsoul13@gmail.com>
2025-02-18 16:20:43 +0530

    Redux Persist added

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import productStore from './redux/store.js'
+import productStore, { persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={productStore}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,35 @@ import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./slices/productSlice"
 import wishSlice from "./slices/wishSlice"
 import cartSlice from "./slices/cartSlice"
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage"
+import { combineReducers } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
 
-const productStore = configureStore({
-    reducer:{
-        productReducer:productSlice,
-        wishReducer:wishSlice,
-        cartReducer:cartSlice
-    }
+
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["wishReducer", "cartReducer"]
+    
+}
+
+const rootReducer = combineReducers({
+    productReducer:productSlice,
+    wishReducer:wishSlice,
+    cartReducer:cartSlice
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const productStore = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, 
+        }),
+})
+
+export const persistor = persistStore(productStore)
 export default productStore 
